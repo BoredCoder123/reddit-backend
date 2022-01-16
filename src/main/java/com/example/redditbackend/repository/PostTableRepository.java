2ce06d696dccf863e6d6fe3cb9343a545607cff5
Ankit Kathait <ankitkathait28@gmail.com>
2022-01-16 20:08:41 +0530

    isbanned bug fix in queries

--- src/main/java/com/example/redditbackend/repository/PostTableRepository.java
+++ src/main/java/com/example/redditbackend/repository/PostTableRepository.java
@@ -9,17 +9,17 @@ import java.util.List;
 
 public interface PostTableRepository extends JpaRepository<PostTable, Integer> {
     @Query(value = "select * from post_table pt where post_id not in " +
-                "(select pt2.post_id from post_table pt2 order by pt2.likes desc, pt2.dislikes asc limit ?1) " +
-            "order by pt.likes desc, pt.dislikes asc limit ?2", nativeQuery = true)
+                "(select pt2.post_id from post_table pt2 where pt2.is_post_banned=false order by pt2.likes desc, pt2.dislikes asc limit ?1) " +
+            "and pt.is_post_banned=false order by pt.likes desc, pt.dislikes asc limit ?2", nativeQuery = true)
     List<PostTable> findTopViewOfAllTime(int start, int end);
 
     @Query(value = "select * from post_table pt where post_id not in " +
-                "(select pt2.post_id from post_table pt2 where pt.post_date > NOW() - interval '1 day' order by pt2.likes desc, pt2.dislikes asc limit ?1)" +
-            " and pt.post_date > NOW() - interval '1 day' order by pt.likes desc, pt.dislikes asc limit ?2", nativeQuery = true)
+                "(select pt2.post_id from post_table pt2 where pt2.is_post_banned=false and pt2.post_date > NOW() - interval '1 day' order by pt2.likes desc, pt2.dislikes asc limit ?1)" +
+            " and pt.post_date > NOW() - interval '1 day' and pt.is_post_banned=false order by pt.likes desc, pt.dislikes asc limit ?2", nativeQuery = true)
     List<PostTable> findTopToday(int start, int end);
 
     @Query(value = "select * from post_table pt where post_id not in " +
-                "(select pt2.post_id from post_table pt2 order by pt.post_date desc limit ?1) " +
-            "order by pt.post_date desc limit ?2", nativeQuery = true)
+                "(select pt2.post_id from post_table pt2 where pt2.is_post_banned=false order by pt.post_date desc limit ?1) " +
+            "and pt.is_post_banned=false order by pt.post_date desc limit ?2", nativeQuery = true)
     List<PostTable> findNew(int start, int end);
 }

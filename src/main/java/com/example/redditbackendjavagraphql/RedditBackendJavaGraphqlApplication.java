76236b87991abde2c458ac25282cb8f623320db3
Ankit Kathait <ankitkathait28@gmail.com>
2022-01-19 10:44:21 +0530

    init

---
+++ src/main/java/com/example/redditbackendjavagraphql/RedditBackendJavaGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.redditbackendjavagraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditBackendJavaGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditBackendJavaGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redditbackendjavagraphql/RedditBackendJavaGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redditbackendjavagraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditBackendJavaGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
